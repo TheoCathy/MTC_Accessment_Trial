1bb6d81f45aa8955badad8408b15833f2500cef7
TheoCathy <theodoranneoma@gmail.com>
2025-06-11 13:13:30 +0100

    Slight build error fixed

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 import footerImg from "../assets/Bitmap_footer.png";
 
 const Footer = () => {
